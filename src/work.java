eddf5eb2dd8caadaedb327c14c60ab93f1cc68d3
user <user@EF07>
2020-09-29 09:37:17 +0800

    print words

--- src/work.java
+++ src/work.java
@@ -1,4 +1,10 @@
 
 public class work {
-
+	
+	public static void main(string[] args) {
+		// TODO Auto-generated method stub
+		system.out.println("I'm Kise");
+		system.out.println("My NID is D0843912");
+		system.out.println("This is Lab 1 hw");
+	}
 }
